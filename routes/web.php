07403de5697c0cc0fea5de9d929ad70c039480a8
Abyan Raga Kusuma <76082121+abyan441@users.noreply.github.com>
2026-08-28 13:25:50 +0700

    Tambahkan route ubah email dan password akun

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,11 @@ use App\Http\Controllers\KelasController;
 use App\Http\Controllers\NilaiController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PenggunaController;
+use App\Http\Controllers\AkunController;
 
 /*
 |--------------------------------------------------------------------------
 | PUBLIC / GUEST
-|--------------------------------------------------------------------------
-| Halaman berikut dapat diakses tanpa login.
-| Guest hanya dapat melihat data.
-|--------------------------------------------------------------------------
-*/
-
-/*
-|--------------------------------------------------------------------------
-| Dashboard
 |--------------------------------------------------------------------------
 */
 
@@ -29,71 +21,18 @@ Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::get('/guru', [GuruController::class, 'index'])->name('guru');
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa');
+Route::get('/kelas', [KelasController::class, 'index'])->name('kelas');
+Route::get('/kelas/{id}/detail', [KelasController::class, 'detail'])->name('kelas.detail');
+Route::get('/mata-pelajaran', [MataPelajaranController::class, 'index'])->name('mapel');
 
-/*
-|--------------------------------------------------------------------------
-| Data Guru - PUBLIC
-|--------------------------------------------------------------------------
-*/
-
-Route::get('/guru', [GuruController::class, 'index'])
-    ->name('guru');
-
-
-/*
-|--------------------------------------------------------------------------
-| Data Siswa - PUBLIC
-|--------------------------------------------------------------------------
-*/
-
-Route::get('/siswa', [SiswaController::class, 'index'])
-    ->name('siswa');
-
-
-/*
-|--------------------------------------------------------------------------
-| Data Kelas - PUBLIC
-|--------------------------------------------------------------------------
-*/
-
-Route::get('/kelas', [KelasController::class, 'index'])
-    ->name('kelas');
-
-Route::get('/kelas/{id}/detail', [KelasController::class, 'detail'])
-    ->name('kelas.detail');
-
-
-/*
-|--------------------------------------------------------------------------
-| Mata Pelajaran - PUBLIC
-|--------------------------------------------------------------------------
-*/
-
-Route::get(
-    '/mata-pelajaran',
-    [MataPelajaranController::class, 'index']
-)->name('mapel');
-
-
-/*
-|--------------------------------------------------------------------------
-| LOGIN
-|--------------------------------------------------------------------------
-*/
-
-Route::get('/login', [LoginController::class, 'index'])
-    ->name('login');
-
-Route::post('/login', [LoginController::class, 'login'])
-    ->name('login.process');
-
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.process');
 
 /*
 |--------------------------------------------------------------------------
 | GURU + ADMIN + KEPALA SEKOLAH
-|--------------------------------------------------------------------------
-| Semua user yang sudah login dan memiliki salah satu role berikut
-| dapat melakukan CRUD data utama.
 |--------------------------------------------------------------------------
 */
 
@@ -102,136 +41,56 @@ Route::middleware([
     'role:guru,admin,kepala_sekolah',
 ])->group(function () {
 
-    /*
-    |--------------------------------------------------------------------------
-    | Data Guru - CRUD
-    |--------------------------------------------------------------------------
-    */
+    Route::post('/guru', [GuruController::class, 'store'])->name('guru.store');
+    Route::put('/guru/{id}', [GuruController::class, 'update'])->name('guru.update');
+    Route::delete('/guru/{id}', [GuruController::class, 'destroy'])->name('guru.destroy');
 
-    Route::post('/guru', [GuruController::class, 'store'])
-        ->name('guru.store');
+    Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+    Route::put('/siswa/{id}', [SiswaController::class, 'update'])->name('siswa.update');
+    Route::delete('/siswa/{id}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
 
-    Route::put('/guru/{id}', [GuruController::class, 'update'])
-        ->name('guru.update');
+    Route::post('/kelas', [KelasController::class, 'store'])->name('kelas.store');
+    Route::put('/kelas/{id}', [KelasController::class, 'update'])->name('kelas.update');
+    Route::delete('/kelas/{id}', [KelasController::class, 'destroy'])->name('kelas.destroy');
 
-    Route::delete('/guru/{id}', [GuruController::class, 'destroy'])
-        ->name('guru.destroy');
+    Route::post('/mata-pelajaran', [MataPelajaranController::class, 'store'])->name('mapel.store');
+    Route::put('/mata-pelajaran/{id}', [MataPelajaranController::class, 'update'])->name('mapel.update');
+    Route::delete('/mata-pelajaran/{id}', [MataPelajaranController::class, 'destroy'])->name('mapel.destroy');
 
-
-    /*
-    |--------------------------------------------------------------------------
-    | Data Siswa - CRUD
-    |--------------------------------------------------------------------------
-    */
-
-    Route::post('/siswa', [SiswaController::class, 'store'])
-        ->name('siswa.store');
-
-    Route::put('/siswa/{id}', [SiswaController::class, 'update'])
-        ->name('siswa.update');
-
-    Route::delete('/siswa/{id}', [SiswaController::class, 'destroy'])
-        ->name('siswa.destroy');
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Data Kelas - CRUD
-    |--------------------------------------------------------------------------
-    */
-
-    Route::post('/kelas', [KelasController::class, 'store'])
-        ->name('kelas.store');
-
-    Route::put('/kelas/{id}', [KelasController::class, 'update'])
-        ->name('kelas.update');
-
-    Route::delete('/kelas/{id}', [KelasController::class, 'destroy'])
-        ->name('kelas.destroy');
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | Mata Pelajaran - CRUD
-    |--------------------------------------------------------------------------
-    */
-
-    Route::post(
-        '/mata-pelajaran',
-        [MataPelajaranController::class, 'store']
-    )->name('mapel.store');
-
-    Route::put(
-        '/mata-pelajaran/{id}',
-        [MataPelajaranController::class, 'update']
-    )->name('mapel.update');
-
-    Route::delete(
-        '/mata-pelajaran/{id}',
-        [MataPelajaranController::class, 'destroy']
-    )->name('mapel.destroy');
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | INPUT NILAI
-    |--------------------------------------------------------------------------
-    */
-
-    Route::get(
-        '/input-nilai',
-        [NilaiController::class, 'index']
-    )->name('input-nilai');
-
-    Route::get(
-        '/input-nilai/data',
-        [NilaiController::class, 'data']
-    )->name('input-nilai.data');
-
-    Route::get(
-        '/input-nilai/siswa',
-        [NilaiController::class, 'siswa']
-    )->name('input-nilai.siswa');
-
-    Route::post(
-        '/input-nilai',
-        [NilaiController::class, 'store']
-    )->name('input-nilai.store');
-
-
-    /*
-    |--------------------------------------------------------------------------
-    | RAPORT
-    |--------------------------------------------------------------------------
-    */
+    Route::get('/input-nilai', [NilaiController::class, 'index'])->name('input-nilai');
+    Route::get('/input-nilai/data', [NilaiController::class, 'data'])->name('input-nilai.data');
+    Route::get('/input-nilai/siswa', [NilaiController::class, 'siswa'])->name('input-nilai.siswa');
+    Route::post('/input-nilai', [NilaiController::class, 'store'])->name('input-nilai.store');
 
     Route::get('/raport', function () {
         return view('raport');
     })->name('raport');
 
     Route::get('/raport/{id}/preview', function ($id) {
-        return view('raport-preview', [
-            'id' => $id,
-        ]);
+        return view('raport-preview', ['id' => $id]);
     })->name('raport.preview');
-
 
     /*
     |--------------------------------------------------------------------------
-    | LOGOUT
+    | PENGATURAN AKUN
+    |--------------------------------------------------------------------------
+    | Semua pengguna yang sudah login dapat mengubah email dan password
+    | miliknya sendiri.
     |--------------------------------------------------------------------------
     */
 
-    Route::post('/logout', [LoginController::class, 'logout'])
-        ->name('logout');
-});
+    Route::put('/akun/email', [AkunController::class, 'updateEmail'])
+        ->name('akun.email.update');
 
+    Route::put('/akun/password', [AkunController::class, 'updatePassword'])
+        ->name('akun.password.update');
+
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+});
 
 /*
 |--------------------------------------------------------------------------
 | ADMIN + KEPALA SEKOLAH
-|--------------------------------------------------------------------------
-| Halaman Pengguna hanya dapat diakses oleh Admin dan Kepala Sekolah.
 |--------------------------------------------------------------------------
 */
 
@@ -240,35 +99,10 @@ Route::middleware([
     'role:admin,kepala_sekolah',
 ])->group(function () {
 
-    Route::get(
-        '/pengguna',
-        [PenggunaController::class, 'index']
-    )->name('pengguna');
-
-    Route::post(
-        '/pengguna',
-        [PenggunaController::class, 'store']
-    )->name('pengguna.store');
-
-    Route::get(
-        '/pengguna/{user}',
-        [PenggunaController::class, 'show']
-    )->name('pengguna.show');
-
-    Route::put(
-        '/pengguna/{user}',
-        [PenggunaController::class, 'update']
-    )->name('pengguna.update');
-
-    Route::patch(
-        '/pengguna/{user}/status',
-        [PenggunaController::class, 'updateStatus']
-    )->name('pengguna.status');
-
-    Route::delete(
-        '/pengguna/{user}',
-        [PenggunaController::class, 'destroy']
-    )->name('pengguna.destroy');
+    Route::get('/pengguna', [PenggunaController::class, 'index'])->name('pengguna');
+    Route::post('/pengguna', [PenggunaController::class, 'store'])->name('pengguna.store');
+    Route::get('/pengguna/{user}', [PenggunaController::class, 'show'])->name('pengguna.show');
+    Route::put('/pengguna/{user}', [PenggunaController::class, 'update'])->name('pengguna.update');
+    Route::patch('/pengguna/{user}/status', [PenggunaController::class, 'updateStatus'])->name('pengguna.status');
+    Route::delete('/pengguna/{user}', [PenggunaController::class, 'destroy'])->name('pengguna.destroy');
 });
-
-
